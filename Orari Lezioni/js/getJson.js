ea2ba3e412509743316b9f02ab6aa58771522799
Unknown <caterina.battisti@studenti.unitn.it>
2017-11-20 12:34:15 +0100

    Predisposizione script in pagina Orari Lezioni

---
+++ Orari Lezioni/js/getJson.js
@@ -0,0 +1,10 @@
+$.ajax({
+   url: "py/getJson.py",
+   success: function(response) {
+   alert("ok");
+	//$.getJSON('path-file-su-server', function(data) {
+	//	obj = JSON.parse(data);
+	//	document.getElementById("demo").innerHTML = obj;
+	//});
+   }
+});
---
+++ Orari Lezioni/py/getJson.py
@@ -0,0 +1,6 @@
+import requests
+
+f=open('orari.txt','r')
+r.requests.get('https://easyroom.unitn.it/Orario/grid_call.php?form-type=corso&anno=2017&corso=0514G&anno2=P0405%7C3&date=20-11-2017&_lang=en&all_events=0')
+r.json()
+f.write(r.text)
