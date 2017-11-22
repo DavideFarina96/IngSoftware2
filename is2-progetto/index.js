98719f38fee4903fdf101591c849b241845a8fa7
Davide Farina <pilota.pazzo@hotmail.it>
2017-11-22 11:22:28 +0100

    aggiunto un path su node

--- is2-progetto/index.js
+++ is2-progetto/index.js
@@ -35,7 +35,6 @@ function CreateBotResponse(msg)
 	botResponse += "\"source\": \"DISIer\"";
 	botResponse += "}";
 
-	console.log(botResponse);
 	return botResponse;
 }
 
@@ -82,8 +81,26 @@ app.all('/orari_e_aule', function(request, response){
 });
 
 //bot-related paths
-app.all('/bot', function(request, response){
-	response.sendfile('bot.html');
+app.all('/bot', function(req, res){
+	res.sendfile('bot.html');
+});
+
+app.all('/bot_content', function(req, response){
+	/*var msg = req.query.msg;
+	var myUrl = "https://api.dialogflow.com/v1/query?v=20150910&contexts=shop&lang=it&query="+ msg +"&sessionId=12345&timezone=Italy/Rome";
+	request({	url: myUrl,  
+			method: "GET",  
+			json: true,
+			headers: {
+					'Authorization': "Bearer 1b94791a77af4cafa8a96f45476d7907"
+				} }, function(error, res, body) {
+				var msgResp = body.result.fulfillment.displayText
+				var msgResp = CreateBotResponse(msgResp);
+
+				response.set('Content-Type', 'application/json');	
+				response.send(msgResp);
+		
+	});*/
 });
 
 // pagina google calendar per visualizzare gli impegni dell'utente
@@ -92,10 +109,68 @@ app.all('/calendar', function(request, response){
 });
 
 
-app.post('/bot_message', function(req, res) {
-	var numeroBus = req.body.result.parameters.busNumber;
-	var citta = req.body.result.parameters.location;
+app.all('/bot_message', function(req, res) {
+	res.set('Content-Type', 'application/json');
+	var numeroBus, citta;
+	var myBody = req.body;
+	
+	console.log(myBody);
 
+	//se arrivo dalla home e non dal bot, faccio la richiesta per avere l'oggetto generalmente creato dal bot.
+	if(myBody.result == null)
+	{
+		var msg = req.query.msg;
+		let myUrl = "https://api.dialogflow.com/v1/query?v=20150910&contexts=shop&lang=it&query="+ msg +"&sessionId=12345&timezone=Italy/Rome";
+			request({	url: myUrl,  
+			method: "GET",  
+			json: true,
+			headers: {
+					'Authorization': "Bearer 1b94791a77af4cafa8a96f45476d7907"
+				} }, function(error, response, body) {
+				if (!error && response.statusCode == 200) 
+				{	
+					myBody = body;
+					numeroBus = myBody.result.parameters.busNumber;
+					citta = myBody.result.parameters.location;
+					var finalMsg = ContinueRequest(numeroBus, citta, res);		
+				}
+				else
+					res.send(CreateBotResponse("errore parsing"));
+			});
+	}
+	else 
+	{
+		numeroBus = myBody.result.parameters.busNumber;
+		citta = myBody.result.parameters.location;
+		var finalMsg = ContinueRequest(numeroBus, citta, res);	
+	}
+	
+});
+
+//-----------------------------
+
+function handleDirections(data){
+		//console.log(data);
+		var times = {time: "00.00", stop: "fermata", line: "0", destination:"_"};
+		for(var i = 0; i < data.routes.length; i++)
+		{
+				for(var j = 0; j<data.routes[i].legs[0].steps.length; j++){
+					if(data.routes[0].legs[0].steps[j].travel_mode == "TRANSIT"){
+						//console.log(data);
+						times.time = data.routes[0].legs[0].steps[j].transit_details.departure_time.text;
+						times.stop = data.routes[0].legs[0].steps[j].transit_details.departure_stop.name;
+						times.line = data.routes[0].legs[0].steps[j].transit_details.line.short_name;
+						times.destination = data.routes[0].legs[0].steps[j].transit_details.arrival_stop.name;
+						return(times);
+					}
+			}
+		}
+		return "-1";
+	
+	}
+
+
+function ContinueRequest(numeroBus, citta, res) {
 	var myUrl = "https://maps.googleapis.com/maps/api/directions/json?origin=Povo&destination=" + citta + "&alternative=true&transit_mode=train|tram|bus&mode=transit&key=AIzaSyBm3HQcIrjGyQAjRemBa9HZjrbp2l0uMCc";
 	var times = "qualcosa";
 
@@ -104,54 +179,33 @@ app.post('/bot_message', function(req, res) {
 				json: true,
 				headers: {
 						'Origin': "https://is2-progetto.herokuapp.com"
-					} }, function(error, response, body) {
-					if (!error && response.statusCode == 200) 
-					{	
-						times = handleDirections(body);
-						var msg = "";	
-						// -1 = no bus.		
-						if(times != "-1")
+						} }, function(error, response, body) {
+						if (!error && response.statusCode == 200) 
 						{	
-							//Aggiungere controllo bus
-							if(numeroBus == times.line)
-								msg = "Il prossimo " + times.line + " per " + citta + " è alle " + times.time + ".";
+							times = handleDirections(body);
+							var msg = "";	
+							// -1 = no bus.		
+							if(times != "-1")
+							{	
+								//Aggiungere controllo bus
+								if(numeroBus == times.line)
+									msg = "Il prossimo " + times.line + " per " + citta + " è alle " + times.time + ".";
+								else
+									msg = "Non c'è il " + numeroBus + ", ma puoi prendere il " + times.line + " alle " + times.time + ".";									
+								
+							}						
 							else
-								msg = "Non c'è il " + numeroBus + ", ma puoi prendere il " + times.line + " alle " + times.time + ".";
-						}						
-						else
-							msg = "Non c'è un autobus per " + citta + ".";						
-						var createdBotResponse = CreateBotResponse(msg);
-						res.set('Content-Type', 'application/json');	
-						res.send(createdBotResponse);
-					}
-					else
-					{ 
-						var createdResponse = CreateBotResponse("errore");
-						res.set('Content-Type', 'application/json');	
-						res.send(createdResponse);	
-					}
-	});
-});
-
-	function handleDirections(data){
-			//console.log(data);
-			var times = {time: "00.00", stop: "fermata", line: "0", destination:"_"};
-			for(var i = 0; i < data.routes.length; i++)
-			{
-					for(var j = 0; j<data.routes[i].legs[0].steps.length; j++){
-						if(data.routes[0].legs[0].steps[j].travel_mode == "TRANSIT"){
-							//console.log(data);
-							times.time = data.routes[0].legs[0].steps[j].transit_details.departure_time.text;
-							times.stop = data.routes[0].legs[0].steps[j].transit_details.departure_stop.name;
-							times.line = data.routes[0].legs[0].steps[j].transit_details.line.short_name;
-							times.destination = data.routes[0].legs[0].steps[j].transit_details.arrival_stop.name;
-							return(times);
+								msg = "Non c'è un autobus per " + citta + ".";						
+							res.send(CreateBotResponse(msg));
 						}
-				}
-			}
-			return "-1";
-		
-		}
+						else
+						{ 
+							res.send(CreateBotResponse("errore"));
+						}
+		});
+}
+
+//-----------------------------------------
 
 //handle requests on /
 app.all('/', function (req, res) {
